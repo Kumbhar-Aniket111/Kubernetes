209f4f430f74bdffe5c650cd6b3a99ad7970c962
Kumbhar-Aniket111 <aniketf062254@gmail.com>
2025-11-23 15:41:14 +0530

    VPA added in Apache web app

--- ApacheWebApp/apache.sh
+++ ApacheWebApp/apache.sh
@@ -3,6 +3,6 @@
 kubectl apply -f apache-namespace.yml
 kubectl config set-context --current --namespace=apache
 kubectl apply -f apache-deployment.yml -f apache-service.yml
-kubectl apply -f apache-hpa.yml
-
+# kubectl apply -f apache-hpa.yml
+kubectl apply -f apache-vpa.yml
 # kubectl port-forward service/apache-svc 100:80 --address=0.0.0.0
